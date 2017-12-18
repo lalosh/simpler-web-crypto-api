c95f4a6b518963415721ffa11c34a4a33bd3e752
Louay Alosh <louayalosh17@gmail.com>
2017-12-18 08:31:33 +0200

    digital cert working well+tests included

--- tests/generateKeys_and_cert.js
+++ tests/generateKeys_and_cert.js
@@ -6,13 +6,13 @@ ds.generateKey()
 
     ds.createCertificate("louayalosh","someorg","unito","li",keyPair)
     .then(function(a){
+
         console.log(a.publicKeyAsPemText);
-        console.log(a.privateKeyAsPemText);
         console.log(a.certAsPemText);
 
-        let pemUrl = "data:application/octet-stream;charset=UTF-8;base64," + btoa(a.certAsPemText);
+        let pemUrl_cert = "data:application/octet-stream;charset=UTF-8;base64," + btoa(a.certAsPemText);
         
-        document.getElementById('cert-pem-download').setAttribute('href',pemUrl);
+        document.getElementById('cert-pem-download').setAttribute('href',pemUrl_cert);
     })
     
 })
